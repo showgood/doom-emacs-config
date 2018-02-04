43d8ab4daab2b05042f4abe8ff80509be71ed697
showgood <showgood21@gmail.com>
2018-02-03 21:41:27 -0500

    use digits to select company-mode candidates

--- config.el
+++ config.el
@@ -392,6 +392,33 @@
 ;; need this to complete something like berlin-crazy-cold-jupiter
 ;; or fifteen_mountain_massachusetts_nineteen
 (setq company-dabbrev-char-regexp "\\(\\sw\\|\\s_\\|_\\|-\\)")
+
+;; https://oremacs.com/2017/12/27/company-numbers/
+(setq company-show-numbers t)
+
+(defun ora-company-number ()
+  "Forward to `company-complete-number'.
+Unless the number is potentially part of the candidate.
+In that case, insert the number."
+  (interactive)
+  (let* ((k (this-command-keys))
+         (re (concat "^" company-prefix k)))
+    (if (cl-find-if (lambda (s) (string-match re s))
+                    company-candidates)
+        (self-insert-command 1)
+      (company-complete-number
+       (if (equal k "0")
+           10
+         (string-to-number k))))))
+
+(let ((map company-active-map))
+  (mapc (lambda (x) (define-key map (format "%d" x) 'ora-company-number))
+        (number-sequence 0 9))
+  (define-key map " " (lambda ()
+                        (interactive)
+                        (company-abort)
+                        (self-insert-command 1)))
+  (define-key map (kbd "<return>") nil))
 ;; ==== END auto-complete settings }}} ====
 
 (require 'fancy-narrow)
